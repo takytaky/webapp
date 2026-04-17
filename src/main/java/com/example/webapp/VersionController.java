3c62b6b82262a547b6d7c7ea27ab1aae2f17743e
Yu mi <takytaky@empas.com>
2026-04-17 04:52:16 +0000

    [V2.0.0] Cloud Native WebApp Demo

--- src/main/java/com/example/webapp/VersionController.java
+++ src/main/java/com/example/webapp/VersionController.java
@@ -13,8 +13,8 @@ public class VersionController {
     @GetMapping("/")
     public String getDashboard(Model model) {
         // CI/CD 테스트 시 아래 두 변수의 값을 변경하며 테스트합니다.
-        String version = "v1.0.0";
-        String bgColor = "#0000ff"; // 초기 색상: 파란색
+        String version = "v2.0.0";
+        String bgColor = "#ff0000"; // 초기 색상: 파란색
 
         // 캐시가 아닌 새로 배포된 서버임을 확인하기 위한 서버 시간
         String serverTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
